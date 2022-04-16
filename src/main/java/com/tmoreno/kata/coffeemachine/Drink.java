13f0dfbb93085f2e5de01ad52e3c92a98ea13db3
tmorenobernal <tmorenobernal@gmail.com>
2022-04-16 21:25:16 +0200

    Added extra hot drinks

--- src/main/java/com/tmoreno/kata/coffeemachine/Drink.java
+++ src/main/java/com/tmoreno/kata/coffeemachine/Drink.java
@@ -4,8 +4,11 @@ import java.math.BigDecimal;
 
 public enum Drink {
     COFFEE("C", new BigDecimal("0.6")),
+    EXTRA_HOT_COFFEE("Ch", new BigDecimal("0.6")),
     TEA("T", new BigDecimal("0.4")),
+    EXTRA_HOT_TEA("Th", new BigDecimal("0.4")),
     CHOCOLATE("H", new BigDecimal("0.5")),
+    EXTRA_HOT_CHOCOLATE("Hh", new BigDecimal("0.5")),
     ORANGE_JUICE("O", new BigDecimal("0.6"));
 
     private final String code;
