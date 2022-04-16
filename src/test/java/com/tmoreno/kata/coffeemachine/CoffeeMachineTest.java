aedf94e67e6c189a5167d6dddebad97f2010aa72
tmorenobernal <tmorenobernal@gmail.com>
2022-04-16 18:32:43 +0200

    Orders has an amount of money

--- src/test/java/com/tmoreno/kata/coffeemachine/CoffeeMachineTest.java
+++ src/test/java/com/tmoreno/kata/coffeemachine/CoffeeMachineTest.java
@@ -19,35 +19,35 @@ public class CoffeeMachineTest {
 
     @Test
     public void should_send_command_to_coffee_maker_for_one_tea_order() {
-        coffeeMachine.make(new Order(Drink.TEA, 0));
+        coffeeMachine.make(new Order(Drink.TEA, 0, Drink.TEA.getPrice()));
 
         verify(coffeeMaker).execute("T::");
     }
 
     @Test
     public void should_send_command_to_coffee_maker_for_one_coffee_order() {
-        coffeeMachine.make(new Order(Drink.COFFEE, 0));
+        coffeeMachine.make(new Order(Drink.COFFEE, 0, Drink.COFFEE.getPrice()));
 
         verify(coffeeMaker).execute("C::");
     }
 
     @Test
     public void should_send_command_to_coffee_maker_for_one_chocolate_order() {
-        coffeeMachine.make(new Order(Drink.CHOCOLATE, 0));
+        coffeeMachine.make(new Order(Drink.CHOCOLATE, 0, Drink.CHOCOLATE.getPrice()));
 
         verify(coffeeMaker).execute("H::");
     }
 
     @Test
     public void should_send_command_to_coffee_maker_for_order_with_one_sugar() {
-        coffeeMachine.make(new Order(Drink.CHOCOLATE, 1));
+        coffeeMachine.make(new Order(Drink.CHOCOLATE, 1, Drink.CHOCOLATE.getPrice()));
 
         verify(coffeeMaker).execute("H:1:0");
     }
 
     @Test
     public void should_send_command_to_coffee_maker_for_order_with_two_sugar() {
-        coffeeMachine.make(new Order(Drink.TEA, 2));
+        coffeeMachine.make(new Order(Drink.TEA, 2, Drink.TEA.getPrice()));
 
         verify(coffeeMaker).execute("T:2:0");
     }
