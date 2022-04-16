d70bb6ffee1de69ba95318c78734c084c8a752a0
tmorenobernal <tmorenobernal@gmail.com>
2022-04-16 17:04:16 +0200

    Added test for one tea order

---
+++ src/test/java/com/tmoreno/kata/coffeemachine/OrderToCommandTest.java
@@ -0,0 +1,17 @@
+package com.tmoreno.kata.coffeemachine;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class OrderToCommandTest {
+
+    @Test
+    public void should_crate_command_for_one_tea_order() {
+        Order order = new Order(TEA, 0);
+
+        String command = order.toCommand();
+
+        assertThat(command).isEqualTo("T::");
+    }
+}
