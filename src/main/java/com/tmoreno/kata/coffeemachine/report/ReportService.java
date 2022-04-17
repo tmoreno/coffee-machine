e775e4e457a0c72738b4833ac6d0ed73d95c7b2e
tmorenobernal <tmorenobernal@gmail.com>
2022-04-17 18:41:34 +0200

    Code to pass test

--- src/main/java/com/tmoreno/kata/coffeemachine/report/ReportService.java
+++ src/main/java/com/tmoreno/kata/coffeemachine/report/ReportService.java
@@ -1,8 +1,13 @@
 package com.tmoreno.kata.coffeemachine.report;
 
+import com.tmoreno.kata.coffeemachine.Order;
 import com.tmoreno.kata.coffeemachine.OrderRepository;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public final class ReportService {
 
@@ -15,6 +20,26 @@ public final class ReportService {
     }
 
     public void execute() {
-        reportWriter.write(new Report(0, 0, 0, 0, BigDecimal.ZERO));
+        List<Order> orders = orderRepository.get();
+
+        Map<String, AtomicInteger> drinkServed = new HashMap<>();
+        BigDecimal totalAmountServed = BigDecimal.ZERO;
+
+        for (Order order : orders) {
+            drinkServed.computeIfAbsent(order.getDrinkCode(), k -> new AtomicInteger())
+                    .incrementAndGet();
+
+            totalAmountServed = totalAmountServed.add(order.getDrinkPrice());
+        }
+
+        Report report = new Report(
+            drinkServed.getOrDefault("T", new AtomicInteger()).intValue(),
+            drinkServed.getOrDefault("C", new AtomicInteger()).intValue(),
+            drinkServed.getOrDefault("H", new AtomicInteger()).intValue(),
+            drinkServed.getOrDefault("O", new AtomicInteger()).intValue(),
+            totalAmountServed
+        );
+
+        reportWriter.write(report);
     }
 }
