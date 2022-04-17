ad4d5412f720db62acbd865c5b25dafaecb69ecc
tmorenobernal <tmorenobernal@gmail.com>
2022-04-17 18:30:18 +0200

    Added test when there are orders

--- src/main/java/com/tmoreno/kata/coffeemachine/OrderRepository.java
+++ src/main/java/com/tmoreno/kata/coffeemachine/OrderRepository.java
@@ -1,7 +1,10 @@
 package com.tmoreno.kata.coffeemachine;
 
+import java.util.List;
+
 public interface OrderRepository {
 
     void save(Order order);
 
+    List<Order> get();
 }
--- src/test/java/com/tmoreno/kata/coffeemachine/ReportServiceTest.java
+++ src/test/java/com/tmoreno/kata/coffeemachine/ReportServiceTest.java
@@ -10,8 +10,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class ReportServiceTest {
@@ -26,11 +28,28 @@ public class ReportServiceTest {
     private ReportService reportService;
 
     @Test
-    public void should_print_empty_when_there_are_not_saved_orders() {
+    public void should_generate_an_empty_report_when_there_are_not_saved_orders() {
         Report emptyReport = new Report(0, 0, 0, 0, BigDecimal.ZERO);
 
         reportService.execute();
 
         verify(reportWriter).write(emptyReport);
+    }
+
+    @Test
+    public void given_one_order_of_each_drink_then_report_has_one_item_sold_of_each_drink_and_total_amount() {
+        when(orderRepository.get())
+            .thenReturn(List.of(
+                new ChocolateOrder(0, false),
+                new CoffeeOrder(0, false),
+                new OrangeJuiceOrder(),
+                new TeaOrder(0, false)
+        ));
+
+        Report report = new Report(1, 1, 1, 1, new BigDecimal("2.1"));
+
+        reportService.execute();
+
+        verify(reportWriter).write(report);
     }
 }
